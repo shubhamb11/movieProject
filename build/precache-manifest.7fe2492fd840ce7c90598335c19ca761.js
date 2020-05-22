a491d7b73d225937d831383f7bdb680fc77d6d4f
Shubham Bhardwaj <Shubham@chalopadho.com>
2020-05-22 08:16:10 +0530

    ghpages build

---
+++ build/precache-manifest.7fe2492fd840ce7c90598335c19ca761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787efa485eba9a453dce92ffd04ccd5d",
+    "url": "/movieProject/index.html"
+  },
+  {
+    "revision": "9847e721be0af1f03f87",
+    "url": "/movieProject/static/css/main.51bf53e2.chunk.css"
+  },
+  {
+    "revision": "60fd294ed26c77a6a30e",
+    "url": "/movieProject/static/js/2.c6dad32c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movieProject/static/js/2.c6dad32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9847e721be0af1f03f87",
+    "url": "/movieProject/static/js/main.29e16eac.chunk.js"
+  },
+  {
+    "revision": "d0bbfb0450db025e365d",
+    "url": "/movieProject/static/js/runtime-main.5c94548b.js"
+  },
+  {
+    "revision": "630af4013de8bee3772278ebf075f334",
+    "url": "/movieProject/static/media/shopping-cart.630af401.svg"
+  }
+]);
